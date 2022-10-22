50843092f3fcc381ca33f7ddbb1bbaf3b4035cde
Dave Spencer <david.spencerian@gmail.com>
2022-10-22 10:34:40 -0700

    break out embedding generation to generate-embeddings.py

--- chess_autoencoder/train.py
+++ chess_autoencoder/train.py
@@ -21,9 +21,6 @@ from tensorflow.keras.losses import BinaryCrossentropy
 
 from tensorflow.keras.metrics import Mean, Accuracy, BinaryAccuracy
 
-import sqlitedict
-
-
 from encode import encode_board, SHAPE, FLAT_SHAPE
 from model import create_models
 
@@ -43,7 +40,8 @@ def generate_training_data(fn):
   print(f'Open {fn}')
   with open(fn, 'r') as f:
     for line in f.readlines():
-      fen, _ = line.split(',')
+      #fen, _ = line.split(',')
+      fen = line.strip()
       board = chess.Board(fen)
       yield encode_board(board)
 
@@ -52,7 +50,8 @@ def generate_training_data2(fn):
   print(f'Open {fn}')
   with open(fn, 'r') as f:
     for line in f.readlines():
-      fen, _ = line.split(',')
+      #fen, _ = line.split(',')
+      fen = line.strip()
       board = chess.Board(fen)
       yield encode_board(board), fen
 
@@ -126,40 +125,8 @@ def main(argv):
 
   train(autoencoder, ds)
 
-  db = sqlitedict.open(filename='embeddings.sqlite',
-                       flag='c',
-                       encode=json.dumps,
-                       decode=json.loads)
-  stack = []
-  fens = []
-  n = 0
-  for encoded, fen in generate_training_data2(fn):
-    if n % 5000 == 0:
-      print('ex', n)
-    n += 1
-    stack.append(encoded)
-    fens.append(fen)
-    if len(stack) == FLAGS.batch:
-      batch = np.stack(stack)
-      embeddings = encoder(batch, training=False)
-
-      for k, v in zip(fens, tf.unstack(embeddings)):
-        db[k] = v.numpy().tolist()
-      stack = []
-      fens = []
-
-  if len(stack) > 0:
-    batch = np.stack(stack)
-    embeddings = encoder(batch, training=False)
-    for k, v in zip(fens, tf.unstack(embeddings)):
-      db[k] = v.numpy().tolist()
-
-  db.commit()
-  db.close()
-  print('done')
-
-
-
+  autoencoder.save('autoencoder.model')
+  encoder.save('encoder.model')
 
 
 
