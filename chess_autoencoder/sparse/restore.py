b66702ce5208f1b6ba8bac7bf640c67d4730a30b
Dave Spencer <david.spencerian@gmail.com>
2024-06-19 13:32:34 -0700

    demo to load checkpoint

---
+++ chess_autoencoder/sparse/restore.py
@@ -0,0 +1,28 @@
+import jax
+from jax.tree_util import tree_structure
+from jax.tree_util import tree_flatten, tree_unflatten
+
+import orbax.checkpoint as ocp
+
+
+
+#c_options = ocp.CheckpointManagerOptions(max_to_keep=3)
+c_path = '/tmp/logdir/checkpoint'
+c_mngr = ocp.CheckpointManager(c_path)
+print('step: ', c_mngr.latest_step())
+restored = c_mngr.restore(
+  c_mngr.latest_step(),
+  args=ocp.args.StandardRestore())
+#print(jax.tree_map(lambda x: x.shape, restored) )
+print()
+print(tree_structure(restored))
+
+print()
+
+def show_example(structured):
+  flat, tree = tree_flatten(structured)
+  unflattened = tree_unflatten(tree, flat)
+  print(f"{structured=}\n  {flat=}\n  {tree=}\n  {unflattened=}")
+
+for foo in restored:
+  show_example(foo)
