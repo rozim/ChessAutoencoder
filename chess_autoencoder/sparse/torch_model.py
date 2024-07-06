caec8a124c37dc66c7bf87c26d97d0a7b2f3c42c
Dave Spencer <david.spencerian@gmail.com>
2024-07-05 18:57:39 -0700

    eval loop

--- chess_autoencoder/sparse/torch_model.py
+++ chess_autoencoder/sparse/torch_model.py
@@ -1,6 +1,7 @@
 from collections import Counter
 import code
 import sys
+import time
 
 import torch
 from torch import nn
@@ -14,14 +15,13 @@ from absl import logging
 
 from schema import *
 
-from torch_data import MyDataset
+from torch_data import MyDataset, FN_TRAIN, FN_TEST
 from torchinfo  import summary
 
 
 FLAGS = flags.FLAGS
 
 
-# Define model
 class MySimpleModel(nn.Module):
   def __init__(self):
     super().__init__()
@@ -76,6 +76,29 @@ class FixedOneHotModel(nn.Module):
     return self.fixed_output.repeat(batch_size, 1)
 
 
+def run_eval(model, device, loss_fn, dl_test):
+  t1 = time.time()
+  total_loss, correct, correct_tot = 0.0, 0, 0
+  with torch.no_grad():
+    for batch, (x, y) in enumerate(dl_test):
+      x = x['board']
+      x, y = x.to(device), y.to(device)
+      pred = model(x)
+      loss = loss_fn(pred, y)
+      total_loss += loss.item()
+      nc = (pred.argmax(1) == y).type(torch.float).sum().item()
+      correct += nc
+      correct_tot += len(x)
+      #if batch % 25 == 0:
+      #print(f'{nc} {correct} {correct_tot} {correct/correct_tot:>6.3f}')
+      if batch >= 100_000:
+        break
+  dt = time.time() - t1
+  print(f'eval correct={correct:,} correct_tot={correct_tot:,} ratio={100.0 * correct / correct_tot:>6.3f}')
+  print(f'loss {total_loss / batch:.1f}')
+  print(f'time: {dt:.1f}s')
+
+
 def main(argv):
 
   device = (
@@ -97,8 +120,10 @@ def main(argv):
   print()
   summary(model, (1, TRANSFORMER_LENGTH,), dtypes=(torch.int32,))
 
-  ds = MyDataset()
-  dl = torch.utils.data.DataLoader(ds, batch_size=64)
+  ds_train = MyDataset(FN_TRAIN)
+  dl_train = torch.utils.data.DataLoader(ds_train, batch_size=64)
+  ds_test = MyDataset(FN_TEST)
+  dl_test = torch.utils.data.DataLoader(ds_test, batch_size=64)
 
   loss_fn = nn.CrossEntropyLoss()
   try:
@@ -110,7 +135,9 @@ def main(argv):
   correct, correct_tot = 0, 0
   y_stats = Counter()
   reports = 0
-  for batch, (x, y) in enumerate(dl):
+  model.train()
+  t1 = time.time()
+  for batch, (x, y) in enumerate(dl_train):
     y_stats.update(y.numpy().tolist())
     x = x['board']
     x, y = x.to(device), y.to(device)
@@ -125,7 +152,7 @@ def main(argv):
       optimizer.step()
       optimizer.zero_grad()
 
-    if batch % 10_00 == 0:
+    if batch % 1000 == 0:
       loss, current = loss.item(), (batch + 1) * len(x)
       print(f"{reports}. loss: {loss:>7f}  [{current:>5d}] {100.0 * correct / correct_tot:>6.3f}")
       reports += 1
@@ -133,28 +160,19 @@ def main(argv):
       correct, correct_tot = 0, 0
       if batch == 100_000:
         break
-        with torch.no_grad():
-          print()
-          for foo in y_stats.most_common(10):
-            print('foo: ', foo)
-          print()
-          for k in sorted(y_stats.keys()):
-            print(k, y_stats[k])
-          print()
-          torch.set_printoptions(threshold=torch.inf)
-          bias_values = model.bias
 
-          print('pred: ', pred)
-          print('pred shape: ', pred.shape)
-          print('bias shape: ', bias_values.shape)
-
-          probabilities = torch.softmax(bias_values, dim=0)
-          print('probs: ', probabilities.shape)
-          print("Bias values:", bias_values)
-          print("Predicted probabilities:", probabilities)
-          print('am: ', probabilities.argmax(-1))
-          sys.exit(0)
-
+  dt = time.time() - t1
+  print(f'train time: {dt:.1f}s')
+  model.eval()
+  print()
+  print('Eval: test')
+  run_eval(model, device, loss_fn, dl_test)
+  print()
+  print('Eval: tain/1 (current place)')
+  run_eval(model, device, loss_fn, dl_train)
+  print()
+  print('Eval: tain/2 (start over)')
+  run_eval(model, device, loss_fn, torch.utils.data.DataLoader(MyDataset(FN_TRAIN), batch_size=64))
 
 
 if __name__ == '__main__':
